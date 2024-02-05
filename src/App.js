26824b428a7f7b56b3240a2e3eb27994d59ef78f
amypo-reactapp <reactapp@amypo.in>
2024-02-05 07:46:50 +0000

    User

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Salman Faris MS 
         </a>
       </header>
     </div>
