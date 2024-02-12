10c858683b65061ac604e6ef91156bc77cfc7286
amypo-reactapp <reactapp@amypo.in>
2024-02-12 11:29:14 +0000

    User

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Salman Faris MS 
+          question1
         </a>
       </header>
     </div>
